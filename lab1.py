71eea383c2d89abacac32725ad14a082d6ecf25a
MatasArlauskas <38531609+MatasArlauskas@users.noreply.github.com>
2025-02-13 23:45:28 +0200

    lab1.py

--- lab1.py
+++ lab1.py
@@ -1,7 +1,5 @@
 import json
 
-#visa sita bloka uzsirasyti kaip stringa ir naudoti python kad ji konvertuoti i dictionary
-
 x = '{"firstName": "Matas", "lastName": "Arlauskas", "age": "22", "city": "Vilnius"}'
 
 y = json.loads(x)
